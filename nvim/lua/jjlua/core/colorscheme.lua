075c5635744bf636153d424974df998264451709
jasonjurotich <jjir.org>
2022-12-09 13:37:44 -0600

    updated

--- nvim/lua/jjlua/core/colorscheme.lua
+++ nvim/lua/jjlua/core/colorscheme.lua
@@ -3,6 +3,6 @@ local colorscheme = "vscode"
 local status, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not status then
-	vim.notify("vscode colorscheme not found.")
-	return
+  vim.notify("vscode colorscheme not found.")
+  return
 end
